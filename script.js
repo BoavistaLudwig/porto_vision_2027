2b257655fabfa7c0c6f18ff86fb723adcb143634
BoavistaLudwig <95169251+BoavistaLudwig@users.noreply.github.com>
2022-04-21 08:03:24 +0100

    Minor

---
+++ script.js
@@ -0,0 +1,9 @@
+const like = document.getElementById("like");
+const share = document.getElementById("share");
+
+like.addEventListener("click", () => {});
+
+share.addEventListener("click", () => {});
+
+console.log(share);
+console.log(like);
